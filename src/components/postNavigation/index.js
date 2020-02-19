6fc762adece1ebe67d688d528c02045105e9de74
felipexperto <felipexperto@hotmail.com>
2020-02-19 17:54:32 -0300

    Passando props para styled components alinhar corretamente o botão de Próximo post caso não houver o de Post Anterior.

--- src/components/postNavigation/index.js
+++ src/components/postNavigation/index.js
@@ -3,14 +3,19 @@ import { Link } from "gatsby";
 
 import * as S from "./styles";
 
+const isItemNull = (item) => item && item !== null ? false : true;
+const navigationAlignmentRight = (arr) => (isItemNull(arr[0]) && !isItemNull(arr[1]));
+
 const PostNavigation = ({previous, next}) => {
 
   return (
     <S.wrapperNavigation>
-      <S.listNavigation>
+      <S.listNavigation
+        alignmentRight={navigationAlignmentRight([previous, next])}
+      >
         {previous && (
           <S.itemNavigationLeft>
-            <Link to={previous.fields.slug} rel="prev">
+            <Link to={previous.fields.slug} rel="anterior">
               <S.linkPreTitle>← Post anterior</S.linkPreTitle>
               <S.linkTitle>{previous.frontmatter.title}</S.linkTitle>
             </Link>
@@ -18,7 +23,7 @@ const PostNavigation = ({previous, next}) => {
         )}
         {next && (
           <S.itemNavigationRight>
-            <Link to={next.fields.slug} rel="next">
+            <Link to={next.fields.slug} rel="próximo">
               <S.linkPreTitle>Próximo post →</S.linkPreTitle>
               <S.linkTitle>{next.frontmatter.title}</S.linkTitle>
             </Link>
