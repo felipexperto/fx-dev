90066eb01b0096c7d63b28c46fce439d6123729c
felipexperto <felipexperto@hotmail.com>
2020-02-18 22:11:06 -0300

    Envelopando o componente corretamente.

--- src/components/postNavigation/index.js
+++ src/components/postNavigation/index.js
@@ -8,22 +8,22 @@ const PostNavigation = ({previous, next}) => {
   return (
     <S.wrapperNavigation>
       <S.listNavigation>
-        <S.itemNavigationLeft>
-          {previous && (
+        {previous && (
+          <S.itemNavigationLeft>
             <Link to={previous.fields.slug} rel="prev">
               <S.linkPreTitle>← Post anterior</S.linkPreTitle>
               <S.linkTitle>{previous.frontmatter.title}</S.linkTitle>
             </Link>
-          )}
-        </S.itemNavigationLeft>
-        <S.itemNavigationRight>
-          {next && (
+          </S.itemNavigationLeft>
+        )}
+        {next && (
+          <S.itemNavigationRight>
             <Link to={next.fields.slug} rel="next">
               <S.linkPreTitle>Próximo post →</S.linkPreTitle>
               <S.linkTitle>{next.frontmatter.title}</S.linkTitle>
             </Link>
-          )}
-        </S.itemNavigationRight>
+          </S.itemNavigationRight>
+        )}
       </S.listNavigation>
     </S.wrapperNavigation>
   )
