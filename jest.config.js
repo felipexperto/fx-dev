c68ae3fde500f959298503a870835926946613f8
Felipe Marciano <felipe.marciano@luizalabs.com>
2022-10-20 08:26:11 -0300

    chore: update jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,24 @@ module.exports = {
   transform: {
     "^.+\\.jsx?$": `<rootDir>/configs/jest-preprocess.js`,
   },
-  "moduleDirectories": ["node_modules", "src"],
-  "roots": [
+  moduleDirectories: ["node_modules", "src"],
+  roots: [
     "<rootDir>/src"
   ],
-  "collectCoverageFrom": [
+  collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}",
     "!<rootDir>/node_modules/",
     "!<rootDir>/src/styles/**/*.js"
   ],
+  coverageReporters: ['lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
   moduleNameMapper: {
     '\\.svg$': '<rootDir>/configs/mockSvgr.js',
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
@@ -23,7 +32,6 @@ module.exports = {
     ], // Workaround for https://github.com/facebook/jest/issues/9771
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `<rootDir>.*/configs`],
-
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: ``,
