71020f44139673f30241b2f5c558b6336d19b502
Felipe Marciano <felipe.marciano@luizalabs.com>
2022-10-20 08:27:27 -0300

    fix: theme provider

--- src/utils/test/theme-provider.js
+++ src/utils/test/theme-provider.js
@@ -1,14 +1,11 @@
-import { React } from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-styled-components';
-import { theme } from '../../styles';
-import { Root } from '../../components/layout';
+import { Root } from 'components/layout';
 
 // eslint-disable-next-line react/prop-types
-const WrapperProvider = ({ children }) => (
-  <Root theme={theme.main}>{children}</Root>
-);
+const WrapperProvider = ({ children }) => <Root>{children}</Root>;
 
 const customRender = (ui, options) =>
   render(ui, { wrapper: WrapperProvider, ...options });
