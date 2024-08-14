8379a7bc42cd2449d61baf5260698f1d16b43d87
Felipe Marciano <felipe.marciano@luizalabs.com>
2024-08-14 18:43:44 -0300

    chore: prettier fix

--- src/components/UI/Avatar/Avatar.test.tsx
+++ src/components/UI/Avatar/Avatar.test.tsx
@@ -6,14 +6,18 @@ import Avatar from './';
 
 describe('[Components] Avatar', () => {
   const renderView = ({ image, label, ...props }: AvatarProps) => {
-    const utils = render(<Avatar image={image} label={label} {...props}/>);
+    const utils = render(<Avatar image={image} label={label} {...props} />);
     return {
       ...utils,
     };
   };
 
   test('should render component properly', () => {
-    const mockProps: AvatarProps = { 'data-testid': 'avatar', image: 'xablau.jpg', label: 'Felipe' };
+    const mockProps: AvatarProps = {
+      'data-testid': 'avatar',
+      image: 'xablau.jpg',
+      label: 'Felipe',
+    };
 
     renderView(mockProps);
     const component = screen.getByTestId('avatar');
