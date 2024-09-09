09aebfdbb9a3eef0b385482cdeb7da50a7555309
Felipe Marciano <felipe.marciano@luizalabs.com>
2024-09-08 21:35:46 -0300

    refactor: rename typography component

---
+++ src/components/UI/Typography/types.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+export type TypographyProps = {
+  children: ReactNode;
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  margin?: string;
+}
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,7 +1,8 @@
 export { default as Avatar } from './Avatar';
 export { default as BannerIllustration } from './BannerIllustration';
+export { default as BoxIcon } from './BoxIcon';
 export { default as Chip } from './Chip';
 export { default as List } from './List';
 export { default as PostCard } from './PostCard';
 export { default as SocialList } from './SocialList';
-export { default as Tipography } from './Tipography';
+export { default as Typography } from './Typography';
