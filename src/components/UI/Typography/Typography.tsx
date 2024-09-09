652db709ad280e356292f1364b0fb1b8058e63bf
Felipe Marciano <felipe.marciano@luizalabs.com>
2024-09-08 21:37:10 -0300

    refactor: rename typography component

---
+++ src/components/UI/Typography/Typography.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TypographyProps } from './types';
+
+import * as S from './styles';
+
+const Typography = ({ children, ...props }: TypographyProps) => (
+  <S.Typography {...props}>
+    {children}
+  </S.Typography>
+);
+
+export default Typography;
